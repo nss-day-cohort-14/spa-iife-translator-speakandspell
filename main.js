16630b9cf65e55cf97cc373c19ddc5fe569b4123
Mike Meadows <mmeadow33@gmail.com>
2016-06-08 14:38:19 -0500

    started French.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var textTranslator = function (french) {
+     var frenchText = {"merry":"joyeux ", 
+     				   "christmas":"noel",
+     				   "and":"et", 
+     				   "happy":"bonne", 
+     				   "new":"nouvel", 
+     				   "year":"An" }
+}();
