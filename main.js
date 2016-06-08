4bd5895a78e8bb634ad9f9aed73f861ecae24ae2
Mike Meadows <mmeadow33@gmail.com>
2016-06-08 15:27:17 -0500

    Bad commit by MM

--- main.js
+++
@@ -1,8 +0,0 @@
-var textTranslator = function (french) {
-     var frenchText = {"merry":"joyeux ", 
-     				   "christmas":"noel",
-     				   "and":"et", 
-     				   "happy":"bonne", 
-     				   "new":"nouvel", 
-     				   "year":"An" }
-}();
