56e04c0ff1d21b91c94eee862a9182603b8e6354
Mike Meadows <mmeadow33@gmail.com>
2016-06-09 11:28:16 -0500

    working translator

--- french.js
+++ french.js
@@ -1,12 +1,12 @@
-var textTranslator = function (french) {
-     var frenchText = {"merry":"joyeux ", 
+var translateText = function (french) {
+     var frenchText = {"merry":"joyeux", 
      				   "christmas":"noel",
      				   "and":"et", 
      				   "happy":"bonne", 
      				   "new":"nouvel", 
      				   "year":"An" };
-     french.transtoToFrench = function(input) {
+     french.translateToFrench = function(input) {
      	return frenchText[input]
      };
 return french;
-}(textTranslator);
+}(translateText);
--- phrase.js
+++ phrase.js
@@ -1,7 +1,7 @@
 // this function will take the string form textInput and covert to an array of words
 
 var phraseToArray = function(inputstring) {
-
+	
 	var inputArray = inputstring.split(" ");
 	return inputArray;
 
